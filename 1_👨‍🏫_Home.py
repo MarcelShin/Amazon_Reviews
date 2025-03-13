defd0b4774a065470e46bc5fdaf06082c700f1d0
Marcelo Vieira de Melo <marcelovieira048@gmail.com>
2025-03-13 20:57:05 -0300

    About me fixed

--- 1_👨‍🏫_Home.py
+++ 1_👨‍🏫_Home.py
@@ -7,10 +7,19 @@ from streamlit_extras.app_logo import add_logo
 st.set_page_config(page_title="Portfólio & Business Analytics", layout="wide")
 
 # Adicionando o logo no body - "Sobre mim"
-st.image("profile_foto_marcelo.jfif", width=150)
 
-st.title("Bem-vindo ao meu Portfólio!")
+col1, col2 = st.columns(2)
 
-st.write("Olá, me chamo Marcelo! Atualmente, atuo na área de Governança de Tecnologia, e utilizo de muitos dados para minhas rotinas de análise.")
+with col1:
+    st.image("profile_foto_marcelo.jfif", width=350)
 
-st.write("Sou apaixonado por tecnologia e sempre em busca de novos desafios, gosto de explorar soluções inovadoras e aprender algo novo a cada dia. Tenho um perfil curioso, analítico e focado em encontrar as melhores estratégias para resolver problemas. Acredito que a inovação e a constante evolução são essenciais para crescer profissionalmente!")
+with col2:
+    st.title("Bem-vindo ao meu Portfólio!")
+    st.write("Olá, me chamo Marcelo! Atualmente, atuo na área de Governança de Tecnologia, e utilizo de muitos dados para minhas rotinas de análise.")
+    st.write("Sou apaixonado por tecnologia e sempre em busca de novos desafios, gosto de explorar soluções inovadoras e aprender algo novo a cada dia. Tenho um perfil curioso, analítico e focado em encontrar as melhores estratégias para resolver problemas. Acredito que a inovação e a constante evolução são essenciais para crescer profissionalmente!")
+
+
+
+
+st.subheader("🎯 Objetivo")
+st.write("Meu objetivo é me desenvolver, enquanto aplico meus conhecimentos no ambiente corporativo e aprimoro minhas skills!")
